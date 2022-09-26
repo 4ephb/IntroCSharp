3c832bb90dc0e5a0e7a7c760d52648e321c1bd42
Alexander_G <alexander.4ephb@gmail.com>
2022-09-27 03:25:27 +1000

    S04T24 Added

---
+++ Seminars/S04T24/Program.cs
@@ -0,0 +1,20 @@
+﻿// 24. Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+// Например:
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+
+
+Console.Clear();
+Console.WriteLine("24. Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.");
+
+int SumOfNumb(int num)
+{
+    int sum = 0;
+    for (int i = 1; i <= num; i++) sum += i;
+    return sum; 
+}
+
+Console.Write("Введите число А: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Сумма чисел от 1 до {A} равна {SumOfNumb(A)}.");
