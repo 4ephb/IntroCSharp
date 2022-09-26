6c06a078c0fb3b65f9c68bef62ffcee134ad24f5
Alexander_G <alexander.4ephb@gmail.com>
2022-09-27 05:50:49 +1000

    S04T30 Added

---
+++ Seminars/S04T30/Program.cs
@@ -0,0 +1,16 @@
+﻿// 30. Напишите программу, которая выводит массив из 8 элементов, заполненный нулями и единицами в случайном порядке.
+// Например:
+// [1,0,1,1,0,1,0,0]
+
+Console.Clear();
+Console.WriteLine("30. Напишите программу, которая выводит массив из 8 элементов, заполненный нулями и единицами в случайном порядке.");
+
+int[] setArr(int len, int min, int max)
+{
+    int[] array = new int[len];
+    for (int i = 0; i < array.Length; i++) array[i] = new Random().Next(min, max);
+    return array;
+}
+
+int[] array = setArr(8, 0, 2);
+Console.WriteLine($"[{String.Join(",", array)}]");
