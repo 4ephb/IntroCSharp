739f0a9eea2d110157c6befc2689771c22b2180e
Alexander_G <alexander.4ephb@gmail.com>
2022-09-27 04:45:22 +1000

    S04T26 Added

---
+++ Seminars/S04T26/Program.cs
@@ -0,0 +1,20 @@
+﻿// 26. Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+// Например:
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+Console.Clear();
+Console.WriteLine("26. Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.");
+
+int NumbCount(int num)
+{
+    int i;
+    for (i = 0; num > 0; i++) num = num / 10;
+    return i;
+}
+
+Console.Write("Введите число: ");
+int N = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+
+Console.WriteLine($"Количество цифр в числе {N} равно {NumbCount(N)}.");
