efd934f6f2fd2d4eb2be67a8cb4a7dccbc3b78d2
Alexander_G <alexander.4ephb@gmail.com>
2022-09-27 07:19:03 +1000

    S04T30 minor fix

--- Seminars/S04T30/Program.cs
+++ Seminars/S04T30/Program.cs
@@ -5,12 +5,12 @@
 Console.Clear();
 Console.WriteLine("30. Напишите программу, которая выводит массив из 8 элементов, заполненный нулями и единицами в случайном порядке.");
 
-int[] setArr(int len, int min, int max)
+int[] SetArr(int len, int min, int max)
 {
     int[] array = new int[len];
     for (int i = 0; i < array.Length; i++) array[i] = new Random().Next(min, max);
     return array;
 }
 
-int[] array = setArr(8, 0, 2);
+int[] array = SetArr(8, 0, 2);
 Console.WriteLine($"[{String.Join(",", array)}]");
