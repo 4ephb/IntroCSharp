ea39b617fc19fcf502b5ebd3bdd068a56b687e01
Alexander_G <alexander.4ephb@gmail.com>
2022-10-03 09:18:15 +1000

    S04T27_HW Added

---
+++ Seminars/S04T27_HW/Program.cs
@@ -0,0 +1,25 @@
+﻿// Домашнее задание
+// 27. Программа принимает на вход число и выдаёт сумму цифр в числе.
+// Например:
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.Clear();
+Console.WriteLine("27. Программа принимает на вход число и выдаёт сумму цифр в числе.");
+
+int SumNumb(int num)
+{
+    int result = 0;
+    num = Math.Abs(num); // int count = Convert.ToString(num).Length;
+    for (int i = 0; num > 0; i++) // for (int i = 0; i < count; i++)
+    {
+        result = result + (num - (num - num % 10));
+        num = num / 10;
+    }
+    return result;
+}
+
+Console.Write("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Сумма цифр числа {N} равна {SumNumb(N)}.");
