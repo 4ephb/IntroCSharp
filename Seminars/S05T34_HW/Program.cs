cdf263237e7e4594f2fc4f1ef734d71bcacea997
Alexander_G <alexander.4ephb@gmail.com>
2022-10-03 10:54:28 +1000

    S05T34_HW Added

---
+++ Seminars/S05T34_HW/Program.cs
@@ -0,0 +1,38 @@
+﻿// Домашнее задание
+// 34. Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+// Например:
+// [345, 897, 568, 234] -> 2
+
+Console.Clear();
+Console.WriteLine($"34. Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.");
+
+int[] FillArray(int size, int min, int max)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+        array[i] = new Random().Next(min, max + 1);
+    return array;
+}
+
+void ShowArray(int[] array)
+{
+    Console.Write("[" + String.Join(", ", array) + "]");
+}
+
+int EvenArrayNum(int[] array)
+{
+    int even = 0;
+    for (int i = 0; i < array.Length; i++)
+        if (array[i] % 2 == 0) even++;
+    return even;
+}
+
+int Size = 5;
+
+int[] array = FillArray(Size, 100, 999);
+ShowArray(array);
+
+Console.Write($" -> ");
+
+int evenNum = EvenArrayNum(array);
+Console.Write($"{evenNum}");
