8096634e19c6474f823893369306b8a5b5673a29
Alexander_G <alexander.4ephb@gmail.com>
2022-10-13 22:20:58 +1000

    S07T46 Added

---
+++ Seminars/S07T46/Program.cs
@@ -0,0 +1,47 @@
+﻿// 46. Задайте двумерный массив размером m×n, заполненный случайными целыми числами.
+// Например:
+// m = 3, n = 4.
+// 1 4 8 19
+// 5 -2 33 -2
+// 77 3 8 1
+
+Console.Clear();
+Console.WriteLine($"46. Задайте двумерный массив размером m×n, заполненный случайными целыми числами.");
+
+int[,] Fill2DArray(int row, int col, int min, int max)
+{
+    int[,] array = new int[row, col];
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            array[i, j] = new Random().Next(min, max + 1);
+        }
+    }
+    return array;
+}
+
+void Show2DArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write(array[i, j] + " ");
+
+        Console.WriteLine();
+    }
+}
+
+Console.Write("Введите количество рядов (m): ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов (n): ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+// Ручной ввод
+// Console.Write("Введите минимальное значение: ");
+// int min = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите максимальное значение: ");
+// int max = Convert.ToInt32(Console.ReadLine());
+
+int[,] Array = Fill2DArray(m, n, 1, 10); // int[,] Array = Fill2DArray(m, n, min, max);
+Show2DArray(Array);
