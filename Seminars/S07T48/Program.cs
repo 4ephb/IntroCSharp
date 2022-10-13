5359feb404ab09f98db9a5c2c635035000c9521c
Alexander_G <alexander.4ephb@gmail.com>
2022-10-14 06:36:25 +1000

    S07T48 Added

---
+++ Seminars/S07T48/Program.cs
@@ -0,0 +1,38 @@
+﻿// 48. Задайте двумерный массив размера m на n, каждый элемент в массиве находится по формуле: Aₘₙ = m+n. Выведите полученный массив на экран.
+// Например:
+// m = 3, n = 4.
+// 0 1 2 3
+// 1 2 3 4
+// 2 3 4 5
+
+Console.Clear();
+Console.WriteLine($"48. Задайте двумерный массив размера m на n, каждый элемент в массиве находится по формуле: Amn = m+n. Выведите полученный массив на экран.");
+
+int[,] Create2DArray(int row, int col)
+{
+    int[,] array = new int[row, col];
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+            array[i, j] = i + j;
+    }
+    return array;
+}
+
+void Show2DArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write("{0, 3}", array[i, j], 2);
+        Console.WriteLine();
+    }
+}
+
+Console.Write("Введите количество рядов (m): ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов (n): ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[,] Array = Create2DArray(m, n);
+Show2DArray(Array);
