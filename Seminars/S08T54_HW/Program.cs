246016c2f0c324f4bb5427a6ede83b7f483fcc17
Alexander_G <alexander.4ephb@gmail.com>
2022-10-17 06:35:52 +1000

    S08T54_HW Added

---
+++ Seminars/S08T54_HW/Program.cs
@@ -0,0 +1,74 @@
+﻿// Домашнее задание
+// 54. Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+Console.Clear();
+Console.WriteLine("54. Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.");
+
+int[,] Fill2DArray(int row, int col, int min, int max)
+{
+    int[,] array = new int[row, col];
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+            array[i, j] = new Random().Next(min, max + 1);
+    }
+    return array;
+}
+
+void Show2DArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write("{0, 3}", array[i, j], 2);
+        Console.WriteLine();
+    }
+}
+
+void RawElemSort(int[,] array)
+{
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        int temp;
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(1) - 1; k++)
+            {
+                if (array[i, k] < array[i, k + 1])
+                {
+                    temp = array[i, k + 1];
+                    array[i, k + 1] = array[i, k];
+                    array[i, k] = temp;
+                }
+            }
+        }
+    }
+}
+
+// Ручной ввод
+// Console.Write("Введите количество рядов (m): ");
+// int m = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите количество столбцов (n): ");
+// int n = Convert.ToInt32(Console.ReadLine());
+
+// Пример
+int m = 3;
+int n = 4;
+
+int[,] Array = Fill2DArray(m, n, 1, 9);
+Console.WriteLine("Исходный массив:");
+Show2DArray(Array);
+
+RawElemSort(Array);
+Console.WriteLine("\nИтоговый массив:");
+Show2DArray(Array);
