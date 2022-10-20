ffd18457b4ed2d29059df61e6ab0a003768f482b
Alexander_G <alexander.4ephb@gmail.com>
2022-10-21 09:49:00 +1000

    S09TP1_HW Added

--- Seminars/S09TP1_HW/Program.cs
+++ Seminars/S09TP1_HW/Program.cs
@@ -6,7 +6,24 @@
 // m = 3, n = 2 -> A(m,n) = 29
 
 Console.Clear();
-Console.WriteLine("");
+Console.WriteLine("Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.");
 
+// Ручной ввод
+// Console.Write("Введите положительное значение m: ");
+// int m = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите положительное значение n: ");
+// int n = Convert.ToInt32(Console.ReadLine());
 
-Console.Write(" -> ");
+// Пример
+int m = 3,
+    n = 2;
+
+int AkkermanFn(int m, int n)
+{
+    if (m == 0) return n + 1;
+    else if (m > 0 && n == 0) return AkkermanFn(m - 1, 1);
+    else return AkkermanFn(m - 1, AkkermanFn(m, n - 1));
+}
+
+int res = AkkermanFn(m, n);
+Console.Write($"m = {m}, n = {n} -> A(m,n) = {res}");
