3f847777cdf5862234bbd0c8891fd6a6014904bb
Alexander_G <alexander.4ephb@gmail.com>
2022-10-21 09:34:14 +1000

    S09T67 Added

---
+++ Seminars/S09T67/Program.cs
@@ -0,0 +1,23 @@
+﻿// 67. Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр с помощью рекурсии.
+// Например:
+// 453 -> 12
+// 45 -> 9
+
+Console.Clear();
+Console.WriteLine("67. Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр с помощью рекурсии.");
+
+// Ручной ввод
+// Console.Write("Введите число: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+
+// Пример
+int n = 453;
+
+int DigitsSumRec(int n)
+{
+    if (n < 10) return n;
+    else return n % 10 + DigitsSumRec(n / 10);
+}
+
+int Sum = DigitsSumRec(n);
+Console.Write($"{n} -> {Sum}");
