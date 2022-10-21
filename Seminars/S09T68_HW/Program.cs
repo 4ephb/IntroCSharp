2de0ce2458464cf90e8df81574df45609543d88d
Alexander_G <alexander.4ephb@gmail.com>
2022-10-21 10:05:26 +1000

    S09TP2_HW Added

--- Seminars/S09T68_HW/Program.cs
+++ Seminars/S09T68_HW/Program.cs
@@ -4,7 +4,23 @@
 // M = 28; N = 7 -> 7
 
 Console.Clear();
-Console.WriteLine("");
+Console.WriteLine("68. Задайте значения M и N. Напишите программу, которая найдёт наибольший общий делитель (НОД) этих чисел с помощью рекурсии.");
 
+// Ручной ввод
+// Console.Write("Задайте значение M: ");
+// int m = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Задайте значение N: ");
+// int n = Convert.ToInt32(Console.ReadLine());
 
-Console.Write(" -> ");
+// Пример
+int m = 28,
+    n = 7;
+
+int NOD(int m, int n)
+{
+    if (m % n == 0) return n;
+    return NOD(n, m % n);
+}
+
+int res = NOD(m, n);
+Console.Write($"M = {m}; N = {n} -> {res}");
