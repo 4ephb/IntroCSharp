9644da8357948fd30a0e306b831b1ca3acb1d386
Alexander_G <alexander.4ephb@gmail.com>
2022-10-21 12:19:39 +1000

    S09TP2_HW Added

--- Seminars/S09TP2_HW/Program.cs
+++ Seminars/S09TP2_HW/Program.cs
@@ -6,7 +6,30 @@
 // N = 5 -> https://ibb.co/9nZgLtY
 
 Console.Clear();
-Console.WriteLine("");
+Console.WriteLine("Напишите программу, которая выводит монотонную последовательность из N элементов в виде равностороннего треугольника с помощью рекурсии");
 
+// Ручной ввод
+// Console.Write("Введите положительное значение N: ");
+// int n = Convert.ToInt32(Console.ReadLine());
 
-Console.Write(" -> ");
+// Пример
+int f = 1,
+    n = 5,
+    m = n;
+
+Console.WriteLine($"N = {n}");
+
+void ShowTriangle(int f, int n)
+{
+    if (f > m) 
+        return;
+    for (int i = 0; i < n - 1; i++) 
+        Console.Write("  ");
+    for (int j = 1; j <= f; j++) 
+        Console.Write("{0, 4}", $"{f}  ");
+
+    Console.WriteLine();
+    ShowTriangle(f + 1, n - 1);
+}
+
+ShowTriangle(f, n);
