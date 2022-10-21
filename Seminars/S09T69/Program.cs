a4746f9c87b23041ff13a7c75adf7ddaa9adbea9
Alexander_G <alexander.4ephb@gmail.com>
2022-10-21 12:50:21 +1000

    S09T69 Added

---
+++ Seminars/S09T69/Program.cs
@@ -0,0 +1,26 @@
+﻿// 69. Напишите программу, которая на вход принимает два числа A и B, и возводит число А в целую степень B с помощью рекурсии.
+// Например:
+// A = 3; B = 5 -> 243 (3⁵)
+// A = 2; B = 3 -> 8
+
+Console.Clear();
+Console.WriteLine("69. Напишите программу, которая на вход принимает два числа A и B, и возводит число А в целую степень B с помощью рекурсии.");
+
+// Ручной ввод
+// Console.Write("Введите число A: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите число B: ");
+// int b = Convert.ToInt32(Console.ReadLine());
+
+// Пример
+int a = 3,
+    b = 5;
+
+int ABPowRec(int a, int b)
+{
+    if (b == 0) return 1;
+    return a * ABPowRec(a, b - 1);
+}
+
+int res = ABPowRec(a, b);
+Console.Write($"A = {a}; B = {b} -> {res}");
